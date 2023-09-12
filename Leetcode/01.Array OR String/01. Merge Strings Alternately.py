f5495677522b4f5ef6d8f9b236db9a7e874cd54b
Aadish1206 <117637995+Aadish1206@users.noreply.github.com>
2023-09-12 23:42:57 +0530

    Update 01. Merge Strings Alternately.py

--- Leetcode/01.Array OR String/01. Merge Strings Alternately.py
+++ Leetcode/01.Array OR String/01. Merge Strings Alternately.py
@@ -1,5 +1,9 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        #print(len(word1))
+        #print(len(word2))
+        
+        #if length of word1 is zero then return word 2 and if word 2 is zero return word 
         if len(word1) == 0:
             return word2
         if len(word2) == 0:
@@ -9,12 +13,14 @@ class Solution:
         a = 0
         b = 0
         
+        #while the variable a and b are less than the lengths of their words they keep combining the letters
         while a < len(word1) and b < len(word2):
             merged += word1[a] + word2[b]
             
             a += 1
             b += 1
-            
+        
+        
         while a < len(word1):
             merged += word1[a]
             a += 1
@@ -24,3 +30,7 @@ class Solution:
             b += 1
             
         return merged
+
+solution = Solution()
+result = solution.mergeAlternately("Aadish", "Ramesh")
+#print(result)
