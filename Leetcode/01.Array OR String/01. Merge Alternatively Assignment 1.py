ddc1ed80b85405877b87aa48c5d6a185240715c1
Aadish1206 <117637995+Aadish1206@users.noreply.github.com>
2023-09-12 23:33:01 +0530

    Create 01. Merge Alternatively Assignment 1.py

---
+++ Leetcode/01.Array OR String/01. Merge Alternatively Assignment 1.py
@@ -0,0 +1,58 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        #print(len(word1))
+        #print(len(word2))
+        if len(word1) == 0:
+            return word2
+        if len(word2) == 0:
+            return word1
+        
+        merged = ""
+        a = 0
+        b = 0
+        
+        #while the variable a and b are less than the lengths of their words they keep combining the letters
+        while a < len(word1) and b < len(word2):
+            merged += word1[a] + word2[b]
+            a += 1
+            b += 1
+            
+        while a < len(word1):
+            merged += word1[a]
+            a += 1
+            
+        while b < len(word2):
+            merged += word2[b]
+            b += 1
+            
+        return merged
+
+class Reverse(Solution):
+    def reverseMerge(self, merged: str) -> str:
+        
+        # Converted the string to swap
+        char_list = list(merged)
+        length = len(char_list)
+        
+        #For loop to swap characters from the beginning and end
+        for i in range(length // 2):
+            char_list[i], char_list[length - i - 1] = char_list[length - i - 1], char_list[i]
+        
+        # Join the reversed characters back into a string
+        reversed_result = ''.join(char_list)
+        return reversed_result
+
+# Create an instance of the Solution class
+solution = Reverse()
+
+
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+merged_result = solution.mergeAlternately(word1, word2)
+
+
+reversed_result = solution.reverseMerged(merged_result)
+
+
+#print("Merged Result:", merged_result)
+#print("Reversed Result:", reversed_result)
