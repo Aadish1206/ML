ff8cbf0c1bc0544176d89ca80c900a06acc3beb5
Aadish1206 <117637995+Aadish1206@users.noreply.github.com>
2023-10-15 00:02:45 +0530

    Create 09. String Compression.py

---
+++ Leetcode/01.Array OR String/09. String Compression.py
@@ -0,0 +1,30 @@
+def compress(chars):
+    nextchar = 0  #We read and write the nextchar pointer
+    read = 0   # Read pointer
+    n = len(chars)
+
+    while read < n:
+        current_char = chars[read]
+        count = 0
+
+        while read < n and chars[read] == current_char:
+            read += 1
+            count += 1
+            #print(current_char)
+          
+        chars[nextchar] = current_char
+        nextchar += 1
+        #print(nextchar)
+      
+        if count > 1:
+            count_str = str(count)
+            for digit in count_str:
+                chars[nextchar] = digit
+                nextchar += 1
+
+    return nextchar
+
+chars = ["a", "a", "b", "b", "c", "c", "c"]
+result_len = compress(chars)
+print(chars[:result_len])  
+print(result_len)         
