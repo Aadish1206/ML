e1c97cdc454ccca4213052286a306fee450f831b
Aadish1206 <117637995+Aadish1206@users.noreply.github.com>
2023-10-21 14:14:39 +0530

    Update 04. Longest subarray of 1s after deleting one element.py

--- Leetcode/03. Sliding Window/04. Longest subarray of 1s after deleting one element.py
+++ Leetcode/03. Sliding Window/04. Longest subarray of 1s after deleting one element.py
@@ -1,20 +1,23 @@
 def longestSubarray(nums):
     max_length = 0
-    left, right = 0, 0  
+    current = 0
+    zero_count = 0
 
-    while right < len(nums):
+    for right in range(len(nums)):
         if nums[right] == 0:
-            left = right  
+            zero_count += 1
+            #print(zero_count)
 
-        max_length = max(max_length, right - left + 1)
-        right += 1
+        while zero_count > 1:
+            if nums[current] == 0:
+                zero_count -= 1
+                #print(zero_count)
+            current += 1
+            #print(current)
+
+        max_length = max(max_length, right - current + 1)
 
     return max_length - 1 
 
-
-nums1 = [1, 1, 0, 1]
-print(longestSubarray(nums1)) 
-
-
-nums2 = [0, 1, 1, 1, 0, 1, 1, 0, 1]
-print(longestSubarray(nums2)) 
+nums = [0, 1, 1, 1, 0, 1, 1, 0, 1]
+print(longestSubarray(nums)) 
